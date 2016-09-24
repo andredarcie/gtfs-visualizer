d730e122df8f4814ef2bd4cb5048e506139a50ff
André N. Darcie <andrendarcie@gmail.com>
2016-09-24 19:00:20 -0300

    Added main file

--- js/get-api.js
+++ js/get-api.js
@@ -1,69 +1,15 @@
-var api_url = "http://127.0.0.1:8000/api/";
-var vez = 0;
-var agency_id = "county-connection";
-var url = api_url + "agency?agency_id=" + agency_id;
-get(url, Generator.drawAgencyStop);
-
-var url = api_url + "routes?agency_id=" + agency_id;
-get(url, Generator.drawRoutes);
-
-function get(url, type) {
+function getApi(url, callback) {
     $.ajax({
         url: url,
         dataType: 'text',
         crossDomain: true,
         type: 'GET',
         success: function(data) {
-            trataResultado(data, type);
+            result(data, callback);
         }
     });
 }
 
-function trataResultado(data, callback) {
+function result(data, callback) {
     callback(data);
-    /*
-    switch (type) {
-        case 'agency':
-            Generator.drawAgencyStop(data);
-            break;
-        case 'routes':
-            Generator.drawRoutes(data);
-            break;
-        case 'return-routes':
-
-            break;
-        case 'shapes':
-            Generator.drawShapes(data);
-            break;
-        case 'stops':
-            Generator.drawStops(data);
-            break;
-        case 'stopsnear':
-            Generator.drawStopsNear(data);
-            break;
-        case 'test':
-            test(data);
-            break;
-    }
-    */
-
-
-}
-
-$("#rotas").click(function() {
-  drawSelectedRoute();
-});
-
-function drawSelectedRoute() {
-
-  map.removeLayer(markers);
-  markers = new L.FeatureGroup();
-
-  //url = api_url + "shapes?agency_key=county-connection&route_id=" + $("#rotas option:selected").val() + "&direction_id=0";
-  //get(url, Generator.drawShapes);
-
-  url = api_url + "stops?agency_key=county-connection&route_id=" + $("#rotas option:selected").val() + "&direction_id=0";
-  console.log(url);
-  get(url, Generator.drawStops);
-
 }
---
+++ js/main.js
@@ -0,0 +1,43 @@
+var settings = {
+  api_url: "http://127.0.0.1:8000/api/",
+  agency_id: "SPTRANS"
+};
+
+start();
+
+function start(){
+
+  var api_url = settings.api_url;
+  var agency_id = settings.agency_id;
+  var url = "";
+
+  url = api_url + "agency?agency_id=" + agency_id;
+  getApi(url, Generator.drawAgencyStop);
+
+  url = api_url + "routes?agency_id=" + agency_id;
+  getApi(url, Generator.drawRoutes);
+
+}
+
+$("#rotas").click(function() {
+  drawSelectedRoute();
+});
+
+function drawSelectedRoute() {
+
+  var api_url = settings.api_url;
+  var agency_id = settings.agency_id;
+  var route_id = $("#rotas option:selected").val();
+  var direction_id = "0";
+  var url = "";
+
+  map.removeLayer(markers);
+  markers = new L.FeatureGroup();
+
+  url = api_url + "shapes?agency_key=" + agency_id + "&route_id=" + route_id + "&direction_id=" + direction_id;
+  getApi(url, Generator.drawShapes);
+
+  url = api_url + "stops?agency_key=" + agency_id + "&route_id=" + route_id + "&direction_id=" + direction_id;
+  getApi(url, Generator.drawStops);
+
+}
